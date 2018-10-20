e3c671d44c5536f21e8f322a30249908635783ba
Steven Kalt <kalt.steven@gmail.com>
2018-10-20 16:40:54 -0400

    reorganized tests for logical consistency

--- test/unit.js
+++ test/unit.js
@@ -13,26 +13,29 @@ import {
   scrape,
 } from '../src/scraper/index.js';
 // import path from 'path';
-import root from 'mdn-browser-compat-data/mathml/elements/math.json';
 import {urlToPath} from '../src/url-to-path';
 import assert from 'assert';
 // import {readFileSync as rfs} from 'fs';
 import {math, atDoc, mouseEnter} from './fixtures';
 import {load} from 'cheerio';
-import Ajv from 'ajv';
 const [$1, $2, $3] = [math, atDoc, mouseEnter]
   .map((html) => load(html, {decodeEntities: false}));
-const logJson = (obj) => console.log(JSON.stringify(obj, null, 2));
-const ajv = new Ajv({allErrors: true});
-import schema from 'mdn-browser-compat-data/schemas/compat-data.schema.json';
+
+// debugging
 import debug from 'debug';
-debug.enable('unit-test:*');
+if (require('process').env.DEBUG) debug.enable('unit-test:*');
 const logger = (name) => debug('unit-test:' + name);
 
+// JSON schema validation
+import Ajv from 'ajv';
+import schema from 'mdn-browser-compat-data/schemas/compat-data.schema.json';
+const ajv = new Ajv({allErrors: true});
 ajv.addSchema(schema, 'main');
+
 const schemaErrorLogger = (ref) => logger(
   'schema:' + ref.constructor === String ? ref : ref.$ref
 );
+
 function testSchema(ref = 'main', json = {}) {
   const log = schemaErrorLogger(ref);
   let valid = ajv.validate(ref, json);
@@ -55,8 +58,8 @@ testSchema.ref = function(ref, json) {
   return testSchema({$ref: `main#/definitions/${ref}`}, json);
 };
 
-describe('urlToPath', ()=>{
-  it('outputs as expected', ()=>{
+describe('URL functions', ()=>{
+  it('translates a url to a path', ()=>{
     let url = '//developer.mozilla.org/en-US/docs/Web/MathML/Element/math';
     assert.equal(urlToPath(url), 'mathml/elements/math');
     url = 'https://developer.mozilla.org/en-US/docs/Web/CSS/@document';
@@ -64,17 +67,17 @@ describe('urlToPath', ()=>{
     url = 'https://developer.mozilla.org/en-US/docs/Web/Events/mouseenter';
     assert.equal(urlToPath(url), 'events/mouseenter');
   });
-});
-describe('checkUrl', ()=>{
-  it('rejects non-mdn hosts', ()=>{
-    assert(!checkUrl({host: 'mozilla.org'}));
+  describe('checkUrl', ()=>{
+    it('rejects non-mdn hosts', ()=>{
+      assert(!checkUrl({host: 'mozilla.org'}));
+    });
+    it('accepts developer.mozilla.org', ()=>{
+      assert(checkUrl({host: 'developer.mozilla.org'}));
+    });
   });
-  it('accepts developer.mozilla.org', ()=>{
-    assert(checkUrl({host: 'developer.mozilla.org'}));
-  });
 });
-describe('getNotes', ()=>{
-  it('returns correctly', ()=>{
+describe('parsing functions', ()=>{
+  it('finds notes correctly', ()=>{
     /* eslint-disable max-len */
     let expected = {
       1: 'Only supported in XHTML documents.',
@@ -94,9 +97,7 @@ describe('getNotes', ()=>{
     };
     assert.deepEqual(assembleNotes($3), expected);
   });
-});
-describe('getBrowserName', ()=>{
-  it('works', ()=>{
+  it('parses browser names', ()=>{
     let {mobile, desktop} = getTables($1);
     let expected = ['chrome', 'edge', 'firefox', 'ie', 'opera', 'safari'];
     assert.deepEqual(getBrowserNames(desktop, $1), expected);
@@ -111,9 +112,7 @@ describe('getBrowserName', ()=>{
     ];
     assert.deepEqual(getBrowserNames(mobile, $1), expected);
   });
-});
-describe('getFeatureNames', ()=>{
-  it('returns as expected', ()=>{
+  it('gets feature names', ()=>{
     let {mobile, desktop} = getTables($1);
     let expected = [
       'Basic support',
@@ -128,45 +127,38 @@ describe('getFeatureNames', ()=>{
     assert.deepEqual(getFeatureNames(mobile, $1), expected);
     assert.deepEqual(getFeatureNames(desktop, $1), expected);
   });
-});
-
-describe('parsing experiment', ()=>{
-  it('can validate stuff', ()=>{
-    testSchema(root);
-    testSchema.ref('identifier', root.mathml);
-    testSchema.ref('identifier', root.mathml.math);
-  });
-  it('correctly parses tables', ()=>{
+  it('parses tables into identifiers', ()=>{
     let table = getTables($1);
     let notes = assembleNotes($1);
     parseTable(table.desktop, notes, $1);
     table = getTables($2);
     let confusing = `
-    <p>
-      61
-      <span class=\"inlineIndicator prefixBox prefixBoxInline\" title=\"prefix\">
-        <a href=\"/en-US/docs/Web/Guide/Prefixes\"
-        title=\"The name of this feature is prefixed with '-moz-' as this
-        browser considers it experimental\">
-          -moz-
-        </a>
-      </span>
-      <sup>
-        <a href=\"#compatNote_1\">1</a>
-      </sup>
-      <sup><a href=\"#compatNote_2\">2</a></sup>
-    </p>
-    <p>
-      1.5 — 61
-      <span class=\"inlineIndicator prefixBox prefixBoxInline\"
-      title=\"prefix\">
-        <a href=\"/en-US/docs/Web/Guide/Prefixes\"
-        title=\"The name of this feature is prefixed with '-moz-' as this
-        browser considers it experimental\">
-          -moz-
-        </a>
-      </span>
-    </p>`;
+      <p>
+        61
+        <span class=\"inlineIndicator prefixBox prefixBoxInline\" title=\"prefix\">
+          <a href=\"/en-US/docs/Web/Guide/Prefixes\"
+          title=\"The name of this feature is prefixed with '-moz-' as this
+          browser considers it experimental\">
+            -moz-
+          </a>
+        </span>
+        <sup>
+          <a href=\"#compatNote_1\">1</a>
+        </sup>
+        <sup><a href=\"#compatNote_2\">2</a></sup>
+      </p>
+      <p>
+        1.5 — 61
+        <span class=\"inlineIndicator prefixBox prefixBoxInline\"
+        title=\"prefix\">
+          <a href=\"/en-US/docs/Web/Guide/Prefixes\"
+          title=\"The name of this feature is prefixed with '-moz-' as this
+          browser considers it experimental\">
+            -moz-
+          </a>
+        </span>
+      </p>
+    `;
     let $ = load(confusing);
     assert.deepEqual(
       parseCell(confusing, {1: 'foo', 2: 'bar'}, $),
@@ -177,35 +169,29 @@ describe('parsing experiment', ()=>{
     );
     notes = assembleNotes($2);
     let json = parseTable(table.desktop, notes, $2);
-    logJson(json);
-    // testSchema.ref('support_block', json);
-    // json = scrape($2);
-    // logJson(json);
-    // testSchema.ref('identifier', root.mathml.elements.math);
     testSchema.ref('identifier', json);
   });
-});
-describe('cell parsing', ()=>{
-  it('correctly parses No', ()=>{
-    const a = `
+  describe('cell parsing', ()=>{
+    it('correctly parses No', ()=>{
+      const a = `
     <td class="no-support">
       <span title="No support">
           No
       </span>
     </td>`;
-    const b = `
+      const b = `
     <td>
       <span>
           No
       </span>
     </td>`;
-    let $ = load(a);
-    assert.deepEqual(parseCell(a, {}, $), {version_added: false});
-    $ = load(b);
-    assert.deepEqual(parseCell(b, {}, $), {version_added: false});
-  });
-  it('correctly parses prefixes', ()=>{
-    const a = `
+      let $ = load(a);
+      assert.deepEqual(parseCell(a, {}, $), {version_added: false});
+      $ = load(b);
+      assert.deepEqual(parseCell(b, {}, $), {version_added: false});
+    });
+    it('correctly parses prefixes', ()=>{
+      const a = `
     <td class="full-support">
       6
       <span class="inlineIndicator prefixBox prefixBoxInline" title="prefix">
@@ -216,19 +202,19 @@ describe('cell parsing', ()=>{
       </span>
     </td>
     `;
-    assert.equal(parseCell(a, {}, $1).prefix, '-moz-');
-  });
-  it('parses unknowns', ()=>{
-    let idk = `
+      assert.equal(parseCell(a, {}, $1).prefix, '-moz-');
+    });
+    it('parses unknowns', ()=>{
+      let idk = `
     <td>
       <span style="color: rgb(255, 153, 0);" title="Compatibility unknown; please update this.">
         ?
       </span>
     </td>`;
-    assert.deepEqual(parseCell(idk, {}, $1), {version_added: null});
-  });
-  it('parses note references', ()=>{
-    let a = `
+      assert.deepEqual(parseCell(idk, {}, $1), {version_added: null});
+    });
+    it('parses note references', ()=>{
+      let a = `
     <td>
       <a href="/en-US/Firefox/Releases/44" title="Released on 2016-01-26.">
         44.0
@@ -236,9 +222,10 @@ describe('cell parsing', ()=>{
       (44.0)
       <sup>[4]</sup>
     </td>`;
-    let expected = '4';
-    let $ = load(a);
-    assert.equal(getNoteReference(a, $), expected);
-    assert.deepEqual(parseCell(a, {4: 'foo'}, $).notes, 'foo');
+      let expected = '4';
+      let $ = load(a);
+      assert.equal(getNoteReference(a, $), expected);
+      assert.deepEqual(parseCell(a, {4: 'foo'}, $).notes, 'foo');
+    });
   });
 });
