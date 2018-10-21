7ef1f2369e168e7a1312969fd6dd41aadbd15060
Steven Kalt <kalt.steven@gmail.com>
2018-10-20 21:18:47 -0400

    eslint

--- src/post-production.js
+++ src/post-production.js
@@ -8,3 +8,6 @@ const bookmarklet = `href="javascript:${encodeURIComponent(expanded.trim())}"`;
 const pageLoc = path.resolve(__dirname, '../dist/index.html');
 let page = fs.readFileSync(pageLoc, 'utf8');
 fs.writeFileSync(pageLoc, page.replace(/href="[^"]+"/, bookmarklet), 'utf8');
+const bundleLoc = path.resolve(__dirname, '../dist/main.js');
+const bundle = fs.readFileSync(bundleLoc, 'utf8');
+fs.writeFileSync(bundleLoc, bundle.replace('main.map', './main.map'), 'utf8');
