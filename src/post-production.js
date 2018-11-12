ccae73b416c084f243bba339bb01b9054187cf31
Steven Kalt <kalt.steven@gmail.com>
2018-11-12 18:31:17 -0500

    minified bookmarklet srtipped whitespace

--- src/post-production.js
+++ src/post-production.js
@@ -3,8 +3,8 @@ const path = require('path');
 const expanded = fs.readFileSync(
   path.resolve(__dirname, 'expanded-bookmarklet.js'),
   'utf8'
-);
-const bookmarklet = `href="javascript:${encodeURIComponent(expanded.trim())}"`;
+).replace(/\s{2,}/, '').trim();
+const bookmarklet = `href="javascript:${encodeURIComponent(expanded)}"`;
 const pageLoc = path.resolve(__dirname, '../dist/index.html');
 let page = fs.readFileSync(pageLoc, 'utf8');
 fs.writeFileSync(pageLoc, page.replace(/href="[^"]+"/, bookmarklet), 'utf8');
