d4370850d103b561309887906e8b2edcf35d4e32
Steven Kalt <kalt.steven@gmail.com>
2018-11-13 22:33:43 -0500

    Added color logging, version-triggered deploy script

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 /* eslint-env browser */
 import {scrape, urlToPath} from './index.js';
-
+import {version} from '../package.json'
 // JSON schema validation
 import Ajv from 'ajv';
 import schema from 'mdn-browser-compat-data/schemas/compat-data.schema.json';
 const ajv = new Ajv({allErrors: true});
 ajv.addSchema(schema, 'main');
-
+console.log(
+  `%c mdn-compat-table-scraper version ${version}`,
+  'background-color: #83d0f2;'
+);
 const path = urlToPath(window.location.href);
 const gh = (p) => `https://github.com/mdn/browser-compat-data/blob/master/${p}`;
 gh.raw = (p) => gh(p).replace('github', 'raw.githubusercontent');
